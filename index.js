8a5c3f6cd29ef7af1c355725641b00f532ce658e
Harsh <harsh.pandey_btech_2024@gsv.ac.in>
2024-04-08 14:04:23 +0530

    second

--- index.js
+++ index.js
@@ -23,16 +23,14 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/train-status', (req, res) => {
-  const train = req.query.trainNo;
-  
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+
+app.get('/train-status', (req, res) => {  
   res.send({
     status: 'success',
     data: data,
     message: 'SUCCESS'
   });
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Example app listening on port 3000!');
 });
